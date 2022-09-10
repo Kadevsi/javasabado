5db5d8836119e5cdaf726f5ca8aa21166408c2cc
Kadevsi <rafkienl@gmail.com>
2022-09-10 11:18:32 -0600

    curso de java basico

---
+++ src/cursosabado/CursoSabado.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursosabado;
+
+
+import java.util.*;
+/**
+ *
+ * @author Rkiehnle
+ */
+public class CursoSabado {
+
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        Scanner num1 = new Scanner (System.in);
+    
+        System.out.println (" ingrese el primer numero");
+        
+        
+        
+        
+    }
+    
+}
